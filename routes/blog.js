f26e5e0aba68dfcfa21fcae5132d58ea8238ea62
Souvikdeb2612 <souvikdeb26@gmail.com>
2021-06-05 02:21:55 +0530

    first commit

--- routes/blog.js
+++ routes/blog.js
@@ -24,11 +24,19 @@ const upload = require("../utils/multer");
 router.post("/single", upload.single("image"), async (req, res) => {
     try {
       // Upload image to cloudinary
-      const result = await cloudinary.uploader.upload(req.file.path);
+      const result = await cloudinary.uploader.upload(req.file.path,{ resource_type: "video", 
+      public_id: "myfolder/mysubfolder/dog_closeup",
+      chunk_size: 6000000,
+      eager: [
+        { width: 300, height: 300, crop: "pad", audio_codec: "none" }, 
+        { width: 160, height: 100, crop: "crop", gravity: "south", audio_codec: "none" } ],                                   
+      eager_async: true,
+      eager_notification_url: "https://mysite.example.com/notify_endpoint" }, function(error, result) {console.log(result, error)});
         // res.json(result)
       // Create new user
       let video = new Blog({
-        name: req.body.name,
+        text: req.body.text,
+        title: req.body.title,
         avatar: result.secure_url,
         cloudinary_id: result.public_id,
       });
